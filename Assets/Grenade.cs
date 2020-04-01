a950dd9cc4550640ad4f2314b09a278d5dbb4c54
Zatch <zmstauffer@gmail.com>
2020-03-31 22:36:47 -0400

    Worked a bit on grenade

--- Assets/Grenade.cs
+++ Assets/Grenade.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
+
+    public float delay = 3;
+    private float timeRemaining;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeRemaining = delay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 0)
+        {
+            explode();
+            Object.Destroy(gameObject);
+        }
+    }
+
+    private void explode()
+    {
+        //show effect
+
+        //add forces to stuff
     }
 }
+
